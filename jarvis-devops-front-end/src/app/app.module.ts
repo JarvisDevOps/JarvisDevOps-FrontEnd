a594fd4d7b1d68b710b6291f5b99e940e138a133
JarvisDevOps <JarvisDevOps@gmail.com>
2018-07-20 10:09:57 +0700

    Add Home and PageNotFound page

--- jarvis-devops-front-end/src/app/app.module.ts
+++ jarvis-devops-front-end/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser'
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { MDBBootstrapModule } from 'angular-bootstrap-md'
+
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [],
